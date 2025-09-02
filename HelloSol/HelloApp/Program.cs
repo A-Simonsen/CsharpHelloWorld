23ab02eb30af1772d10497d34207939316f71056
A-Simonsen <10632082@ucn.dk>
2025-09-02 21:33:40 +0200

    Update Program.cs

--- HelloSol/HelloApp/Program.cs
+++ HelloSol/HelloApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your name:");
+String input = Console.ReadLine();
+Console.WriteLine("Hello, " + input + "!");

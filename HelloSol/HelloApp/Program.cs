3c86b37acd67abafc285b706cf53f426f5c17fc6
A-Simonsen <10632082@ucn.dk>
2025-09-02 21:37:32 +0200

    Add user prompt and greeting to Program.cs
    Program now asks the user to enter their name and greets them using the provided input. Added comments for clarity.

--- HelloSol/HelloApp/Program.cs
+++ HelloSol/HelloApp/Program.cs
@@ -1,4 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+///
+///Bare et simpelt C# program der beder brugeren om at indtaste sit navn og derefter hilser på brugeren med det indtastede navn.
+///
+
+// Printer en besked som beder brugeren om at indtaste sit navn
 Console.WriteLine("Enter your name:");
+// Tager input fra brugeren
 String input = Console.ReadLine();
+// Tager input og printer en hilsen
 Console.WriteLine("Hello, " + input + "!");
